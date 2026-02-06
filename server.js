b19e3515855cdfbd33dde86e3393a047215a045f
danyapro279 <167842879+danyapro279@users.noreply.github.com>
2026-02-06 13:36:39 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -1,42 +1,30 @@
 const express = require('express');
-const fs = require('fs');
+const http = require('http');
+const { Server } = require('socket.io');
 const path = require('path');
-const app = express();
-const PORT = 3000;
-const DATA_FILE = path.join(__dirname, 'data.json');
 
-app.use(express.json());
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Отдаем файл index.html, когда заходим на главную
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Это нужно, чтобы сервер разрешал подгружать скрипты/картинки из этой же папки
 app.use(express.static(__dirname));
 
-// Инициализация файла данных
-if (!fs.existsSync(DATA_FILE)) {
-    fs.writeFileSync(DATA_FILE, JSON.stringify({ sun: 0, cloud: 0, rain: 0, dots: [] }));
-}
+let votes = [];
 
-// Получить все данные
-app.get('/api/data', (req, res) => {
-    try {
-        const data = JSON.parse(fs.readFileSync(DATA_FILE));
-        res.json(data);
-    } catch (e) {
-        res.status(500).json({ error: "Ошибка чтения данных" });
-    }
+io.on('connection', (socket) => {
+    socket.emit('init_votes', votes);
+    socket.on('new_vote', (vote) => {
+        votes.push(vote);
+        io.emit('add_vote', vote);
+    });
 });
 
-// Сохранить реакцию
-app.post('/api/react', (req, res) => {
-    const { type, x, y } = req.body;
-    try {
-        const data = JSON.parse(fs.readFileSync(DATA_FILE));
-        data[type]++;
-        data.dots.push({ type, x, y, id: Date.now() });
-        fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
-        res.json({ success: true });
-    } catch (e) {
-        res.status(500).json({ error: "Ошибка записи данных" });
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`Сервер летит на: http://localhost:${PORT}`);
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Сервер работает на порту 3000');
 });
